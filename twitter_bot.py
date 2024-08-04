be1bcde433e462910cb06078a63c3fe0a4db5c9c
Susmit Kumar Pandey <susmitpandey22@gmail.com>
2024-08-04 22:55:37 +0530

    added wrong credentials login

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,12 +11,14 @@ promised_down = 150
 promised_up = 10
 
 
-t_user = 'asgu21is@cmrit.ac.in'
-t_pwd = 'testingtest'
+t_user = ''
+t_pwd = ''
+wrong_user = 'wrong_user@cmrit.ac.in'
+wrong_pwd = 'wrongpassword'
 
 
 class InternetSpeedTwitterBot:
-    def __init__(self):
+    def _init_(self):
         self.driver = webdriver.Chrome()
         self.down = 0
         self.up = 0
@@ -48,42 +50,57 @@ class InternetSpeedTwitterBot:
             print(self.down)
         except NoSuchElementException as e:
             print(e)
-        finally:
-            self.driver.quit()
 
-    def tweet_at_provider(self):
+    def tweet_at_provider(self, username, password):
         self.driver.maximize_window()
         self.driver.get(
             'https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoiZW4ifQ%3D%3D%22%7D')
         time.sleep(5)
         try:
-            user = self.driver.find_element(By.NAME, 'text')
-            user.send_keys(t_user)
+            user = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.NAME, 'text'))
+            )
+            user.send_keys(username)
             user.send_keys(Keys.ENTER)
-            time.sleep(3)
 
-            pwd = self.driver.find_element(By.NAME, 'password')
-            pwd.send_keys(t_pwd)
+            pwd = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.NAME, 'password'))
+            )
+            pwd.send_keys(password)
             pwd.send_keys(Keys.ENTER)
+
             time.sleep(7)
 
-            self.driver.find_element(
-                By.XPATH, '//*[@id="react-root"]/div/div/div[2]/header/div/div/div/div[1]/div[3]/a').click()
+            if "login error" in self.driver.page_source.lower():
+                print("Login failed. Check your username and password.")
+                return
+
+            tweet_button = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, '//*[@id="react-root"]/div/div/div[2]/header/div/div/div/div[1]/div[3]/a'))
+            )
+            tweet_button.click()
             time.sleep(5)
 
             pt.write(
-                "Hey Twitter. wHY MY iNTERNET speed is so slow when I am paying good money.")
+                "Hey Twitter. WHY MY INTERNET speed is so slow when I am paying good money.")
             time.sleep(5)
 
             pt.click(x=1352, y=627)
             # print(pt.position())
-            time.sleep(2)
-        except NoSuchElementException as e:
+            time.sleep(5)
+        except (NoSuchElementException, TimeoutException) as e:
             print(e)
-        finally:
-            self.driver.quit()
+        # finally:
+        #     self.driver.quit()
 
 
 bot = InternetSpeedTwitterBot()
-bot.get_internet_speed()
-bot.tweet_at_provider()
+# bot.get_internet_speed()
+
+print("Testing with incorrect credentials:")
+bot.tweet_at_provider(wrong_user, wrong_pwd)
+print("The credentials are incorrect.")
+
+print("Testing with correct credentials:")
+bot.tweet_at_provider(t_user, t_pwd)
